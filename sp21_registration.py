a3af4dfbed209b68d03211c1241e07f8a351f0ba
anitejb <41495799+anitejb@users.noreply.github.com>
2020-11-22 00:20:56 -0500

    added flipped courses to log

--- sp21_registration.py
+++ sp21_registration.py
@@ -18,6 +18,9 @@ def update_db(db, all_sections, open_sections):
         current_db = dict()
     latest_db = dict.fromkeys(all_sections, {})
 
+    flipped_open = []
+    flipped_closed = []
+
     for section in all_sections:
         if check_section_is_open(section, open_sections):
             latest_db[section]["status"] = 1
@@ -26,6 +29,7 @@ def update_db(db, all_sections, open_sections):
             )
             if section not in current_db or current_db[section]["status"] == 0:
                 latest_db[section]["flip"].append(timestamp)
+                flipped_open.append(section)
         else:
             latest_db[section]["status"] = 0
             latest_db[section]["flip"] = (
@@ -33,9 +37,12 @@ def update_db(db, all_sections, open_sections):
             )
             if section not in current_db or current_db[section]["status"] == 1:
                 latest_db[section]["flip"].append(timestamp)
+                flipped_closed.append(section)
 
     latest_db["timestamps"] = current_db.get("timestamps", []) + [timestamp]
     db.update(latest_db)
+
+    print(f"Updated @ {int(time.time())} | Flipped Open: {flipped_open} | Flipped Closed: {flipped_closed}")
 
 
 def main():
@@ -45,7 +52,6 @@ def main():
     open_sections = get_open_sections()
 
     update_db(db, all_sections, open_sections)
-    print(f"Updated @ {int(time.time())}")
 
 
 def shutdown(event):
@@ -56,6 +62,8 @@ def shutdown(event):
 
 if __name__ == "__main__":
     print(f"Started @ {int(time.time())}")
+    # Initial cron trigger
+    main()
     # Set cron function to be executed in intervals of 10 minutes
     scheduler = BlockingScheduler()
     scheduler.add_listener(shutdown, EVENT_JOB_ERROR)
